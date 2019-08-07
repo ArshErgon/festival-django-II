2e4b5d286f18519fd48120d1ca84e44e06ef6c46
ArshErgon <pgcomputer7@gmail.com>
2019-08-07 23:58:36 +0530

    'done'

--- eid/settings.py
+++ eid/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,8 +25,8 @@ SECRET_KEY = 'j8o&lkb)6%@t5820#32#0%ffzw_o^@!$f4napj4@y*#_r$3rgr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+# DEBUG_PROPAGATE_EXCEPTIONS = True
+ALLOWED_HOSTS = ['.herokuapp.com', 'bakraeid.herokuapp.com', '*']
 
 
 # Application definition
@@ -119,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
